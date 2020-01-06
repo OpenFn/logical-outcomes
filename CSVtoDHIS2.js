953ddc9e25ef1c33aab49a0898a0b058cc863bd1
Taylor Downs <downs.taylor@gmail.com>
2020-01-06 20:41:05 +0000

    td concept

--- CSVtoDHIS2.js
+++ CSVtoDHIS2.js
@@ -1,52 +1,49 @@
-createTEI(
-  fields(
-    field("trackedEntityType", dataValue("trackedEntity")(state)),
-    field("orgUnit", dataValue("csvData.Org Unit UID")(state)),
-    field("attributes", function(state){
-      return [{
-          "C1O4lnx0Ibz": dataValue("PARTNER")(state), //Partner category
-          "bnQOk4Plo9Z": dataValue("bnQOk4Plo9Z")(state), //UTNWF Staff
-          "TLjYqcKwhxP": dataValue("TLjYqcKwhxP")(state), //Beneficiary Name
-          "ScSWoiqvdp5": dataValue("ScSWoiqvdp5")(state), //ID No
-          "j38AUmNNy18": dataValue("j38AUmNNy18")(state), //Age
-          "F2FjwHE7MTs": dataValue("F2FjwHE7MTs")(state), //Sex
-          "g5QDO6GPgiJ": dataValue("g5QDO6GPgiJ")(state), //Telephone
-          "zC1KsiGDVMk": dataValue("zC1KsiGDVMk")(state), //Village
-          "cUqyRxPt1U7": dataValue("cUqyRxPt1U7")(state) //Household head --> wrong Id, CONVERT TO BOOLEAN
-      }]
-  }),
-    field("enrollments", function(state){
-     return [{
-       "program": "NGtZYxE0zFM", //FARM SPECIFIC ACTION PLAN?
-       "programStage": "wewcqVyGL5z", //FARM SPECIFIC ACTION PLAN?
-       "orgUnit": dataValue("csvData.Org Unit UID")(state),
-       "enrollmentDate": dataValue("START DATE (MM/YY)")(state),
-       "incidentDate": dataValue("START DATE (MM/YY)")(state)
-     }]
-   })
-  )
-),
-event(
-  fields(
-    field("program", "NGtZYxE0zFM"), //FARM SPECIFIC ACTION PLAN??
-    field("programStage", "wewcqVyGL5z"), //FARM SPECIFIC ACTION PLAN?
-    field("orgUnit", dataValue("csvData.Org Unit UID")(state)),
-    field("eventDate", dataValue("START DATE (MM/YY)"))(state),
-    field("status", "COMPLETED"),
-    field("storedBy", "Test"),
-    field("orgUnitName", dataValue("Org unit")(state)),
-    field("attributeCategoryOptions", "vwGC12Ipn10"),
-    field("optionSize",0),
-    field("attributeOptionCombo","C1O4lnx0Ibz"),
-    field("coordinate", {
-      "latitude": dataValue("LATITUDE (NORTH+/ SOUTH-)")(state),
-      "longitude": dataValue("LATITUDE (EAST/ WEST)")(state)
-    }),
-    field("dataValues", function(state){
-      return [{
-        "dataElement": "vAh7VEB6L0f",
-        "value": dataValue("vAh7VEB6L0f")(state)
-      }]
-    })
-  )
-)
+createTEI({
+  trackedEntityType: dataValue('trackedEntity')(state),
+  orgUnit: dataValue('csvData.Org Unit UID')(state),
+  attributes: [
+    {
+      C1O4lnx0Ibz: dataValue('csvData.PARTNER')(state), //Partner category
+      bnQOk4Plo9Z: dataValue('csvData.bnQOk4Plo9Z')(state), //UTNWF Staff
+      TLjYqcKwhxP: dataValue('csvData.TLjYqcKwhxP')(state), //Beneficiary Name
+      ScSWoiqvdp5: dataValue('csvData.ScSWoiqvdp5')(state), //ID No
+      j38AUmNNy18: dataValue('csvData.j38AUmNNy18')(state), //Age
+      F2FjwHE7MTs: dataValue('csvData.F2FjwHE7MTs')(state), //Sex
+      g5QDO6GPgiJ: dataValue('csvData.g5QDO6GPgiJ')(state), //Telephone
+      zC1KsiGDVMk: dataValue('csvData.zC1KsiGDVMk')(state), //Village
+      cUqyRxPt1U7: dataValue('csvData.cUqyRxPt1U7')(state), //Household head --> wrong Id, CONVERT TO BOOLEAN
+    },
+  ],
+  enrollments: [
+    {
+      program: 'NGtZYxE0zFM', //FARM SPECIFIC ACTION PLAN?
+      programStage: 'wewcqVyGL5z', //FARM SPECIFIC ACTION PLAN?
+      orgUnit: dataValue('csvData.Org Unit UID')(state),
+      enrollmentDate: dataValue('csvData.START DATE (MM/YY)')(state),
+      incidentDate: dataValue('csvData.START DATE (MM/YY)')(state),
+    },
+  ],
+});
+
+event({
+  program: 'NGtZYxE0zFM', //FARM SPECIFIC ACTION PLAN??
+  programStage: 'wewcqVyGL5z', //FARM SPECIFIC ACTION PLAN?
+  orgUnit: dataValue('csvData.Org Unit UID')(state),
+  eventDate: dataValue('START DATE (MM/YY)')(state),
+  status: 'COMPLETED',
+  storedBy: 'Test',
+  orgUnitName: dataValue('Org unit')(state),
+  attributeCategoryOptions: 'vwGC12Ipn10',
+  optionSize: 0,
+  attributeOptionCombo: 'C1O4lnx0Ibz',
+  coordinate: {
+    latitude: dataValue('LATITUDE (NORTH+/ SOUTH-)')(state),
+    longitude: dataValue('LATITUDE (EAST/ WEST)')(state),
+  },
+  dataValues: [
+    {
+      dataElement: 'vAh7VEB6L0f',
+      value: dataValue('vAh7VEB6L0f')(state),
+    },
+  ],
+});
