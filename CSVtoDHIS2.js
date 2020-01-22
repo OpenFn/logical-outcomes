972291c0b1e12293990752914740b0551d83c409
aleksa-krolls <aleksagk@gmail.com>
2020-01-22 16:47:29 +0200

    updated job with new http adapator

--- CSVtoDHIS2.js
+++ CSVtoDHIS2.js
@@ -37,9 +37,9 @@ createTEI({
       enrollmentDate: dataValue('csvData.START DATE (MM/YY)')(state).toString().replace(/\//g,"-"),
       incidentDate: dataValue('csvData.START DATE (MM/YY)')(state).toString().replace(/\//g,"-"),
       events: [
-        {
-          program: 'NGtZYxE0zFM', //FARM SPECIFIC ACTION PLAN??
-          programStage: 'wewcqVyGL5z', //FARM SPECIFIC ACTION PLAN?
+        { //Enrollment 1
+          program: 'NGtZYxE0zFM', //FARM SPECIFIC ACTION PLAN
+          programStage: 'wewcqVyGL5z', //FARM SPECIFIC ACTION PLAN
           orgUnit: dataValue('csvData.Org Unit UID')(state),
           eventDate: dataValue('csvData.START DATE (MM/YY)')(state).toString().replace(/\//g,"-"),
           status: 'COMPLETED',
@@ -53,7 +53,7 @@ createTEI({
             longitude: dataValue('csvData.LATITUDE (EAST/ WEST)')(state),
           },
           dataValues: [
-          {
+          { //Col Q
             dataElement: 'vAh7VEB6L0f',
             value: state => {
               var value = dataValue('csvData.vAh7VEB6L0f')(state);
@@ -61,9 +61,235 @@ createTEI({
               return (val==="yes"? true : false);
             }
           },
+          { //Col R
+            dataElement: 'IyW6h3oi1Gd',
+            value: dataValue('IyW6h3oi1Gd')(state)
+          },
+          { //Enrollment 2
+            program: 'NGtZYxE0zFM',
+            programStage: 'wewcqVyGL5z', //TECHNOLOGY ADOPTION
+            orgUnit: dataValue('csvData.Org Unit UID')(state),
+            eventDate: dataValue('csvData.START DATE (MM/YY)')(state).toString().replace(/\//g,"-"),
+            status: 'COMPLETED',
+            storedBy: 'Test',
+            orgUnitName: dataValue('csvData.Org unit')(state),
+            attributeCategoryOptions: dataValue('csvData.Category option UID')(state),
+            optionSize: 0,
+            attributeOptionCombo: 'C1O4lnx0Ibz',
+            coordinate: {
+              latitude: dataValue('csvData.LATITUDE (NORTH+/ SOUTH-)')(state),
+              longitude: dataValue('csvData.LATITUDE (EAST/ WEST)')(state),
+            },
+            dataValues: [
+            { //Col S
+              dataElement: 'LZ0FOPRvtdm',
+              value: dataValue('LZ0FOPRvtdm')(state),
+              }
+            },
+            { //Col T
+              dataElement: 'kP5o7Ud1Vd4',
+              value: dataValue('kP5o7Ud1Vd4')(state)
+            },
+            { //Col U
+              dataElement: 'uFUTTqkEVtz',
+              value: dataValue('uFUTTqkEVtz')(state)
+            },
+            { //Col V
+              dataElement: 'JIb0HKi7MLq',
+              value: dataValue('JIb0HKi7MLq')(state)
+            },
+            { //Col W
+              dataElement: 'x7X1y7b4tAu',
+              value: dataValue('x7X1y7b4tAu')(state)
+            },
+            { //Col X
+              dataElement: 'VMyJ10uue3t',
+              value: dataValue('VMyJ10uue3t')(state)
+            },
+            { //Col Y
+              dataElement: 'yH9GVEG6Pya',
+              value: dataValue('yH9GVEG6Pya')(state)
+            },
+            { //Col Z
+              dataElement: 'k7ZrxD00oTk',
+              value: dataValue('k7ZrxD00oTk')(state)
+            },
+            { //Col AA
+              dataElement: 'wvKDeTzKnOw',
+              value: dataValue('wvKDeTzKnOw')(state)
+            },
+            { //Col AB
+              dataElement: 'fohag7LHogI',
+              value: dataValue('fohag7LHogI')(state)
+            },
+            { //Col AC
+              dataElement: 'gkFvMlQR7mD',
+              value: dataValue('gkFvMlQR7mD')(state)
+            },
+            { //Col AD
+              dataElement: 'GUjfts8o2I3',
+              value: dataValue('GUjfts8o2I3')(state)
+            },
+            { //Col AE
+              dataElement: 'oiiWce30HFA',
+              value: dataValue('oiiWce30HFA')(state)
+            },
+            { //Col AF
+              dataElement: 'RJDAG9P1o68',
+              value: dataValue('RJDAG9P1o68')(state)
+            },
+            { //Col AG
+              dataElement: 'Vl0MZ8bG3Mj',
+              value: dataValue('Vl0MZ8bG3Mj')(state)
+            },
+            { //Col AH
+              dataElement: 'ZH28Ef03rWf',
+              value: dataValue('ZH28Ef03rWf')(state)
+            },
+            { //Col AI
+              dataElement: 'NgIYWTQIG1k',
+              value: dataValue('NgIYWTQIG1k')(state)
+            },
+            { //Col AJ
+              dataElement: 'bnpFpibSHTZ',
+              value: dataValue('bnpFpibSHTZ')(state)
+            },
+            { //Col AK
+              dataElement: 'JE8QDXFNPQ3',
+              value: dataValue('JE8QDXFNPQ3')(state)
+            },
+            { //Col AL
+              dataElement: 'IXBQ9iyOsj7',
+              value: dataValue('IXBQ9iyOsj7')(state)
+            },
+            { //Col AM
+              dataElement: 'CdGdpMnhq9N',
+              value: dataValue('CdGdpMnhq9N')(state)
+            },
+            { //Col AN
+              dataElement: 'CDK1lWPutjS',
+              value: dataValue('CDK1lWPutjS')(state)
+            },
+            { //Col AO
+              dataElement: 'yoS2hi15LNw',
+              value: dataValue('yoS2hi15LNw')(state)
+            },
+            { //Col AP
+              dataElement: 'wIora0v6L1w',
+              value: dataValue('wIora0v6L1w')(state)
+            },
+            { //Col AQ
+              dataElement: 'lSgPvELDL1f',
+              value: dataValue('lSgPvELDL1f')(state)
+            },
+            { //Col AR
+              dataElement: 't65Q6g7ZyDH',
+              value: dataValue('t65Q6g7ZyDH')(state)
+            },
+            { //Col AS
+              dataElement: 'dtgxdpuv1gi',
+              value: dataValue('dtgxdpuv1gi')(state)
+            },
+            { //Col AT
+              dataElement: 'YZXBjzU3qq6',
+              value: dataValue('YZXBjzU3qq6')(state)
+            },
+            { //Col AU
+              dataElement: 'y7ffvo37Unv',
+              value: dataValue('y7ffvo37Unv')(state)
+            },
+            { //Col AV
+              dataElement: 'LmWgNlOgEmJ',
+              value: dataValue('LmWgNlOgEmJ')(state)
+            },
+            { //Col AW
+              dataElement: 'iADbtBlMxTs',
+              value: dataValue('iADbtBlMxTs')(state)
+            },
+            { //Col AX
+              dataElement: 'sYUhjK3UdP1',
+              value: dataValue('sYUhjK3UdP1')(state)
+            },
+            { //Col AY
+              dataElement: 'l4pKaxmLOrW',
+              value: dataValue('l4pKaxmLOrW')(state)
+            },
+            { //Col AZ
+              dataElement: 'otlK2nqPJd1',
+              value: dataValue('otlK2nqPJd1')(state)
+            },
+            { //Col BA
+              dataElement: 'frjA2JXh9cY',
+              value: dataValue('frjA2JXh9cY')(state)
+            },
+            { //Col BB
+              dataElement: 'Eh89gu65Xnd',
+              value: dataValue('Eh89gu65Xnd')(state)
+            },
+            { //Col BC
+              dataElement: 'I3mYNeMDixy',
+              value: dataValue('I3mYNeMDixy')(state)
+            },
+            { //Col BD
+              dataElement: 'fK3Pqv7IWo7',
+              value: dataValue('fK3Pqv7IWo7')(state)
+            },
+            { //Col BE
+              dataElement: 'PrPEHhALnzl',
+              value: dataValue('PrPEHhALnzl')(state)
+            },
+            { //Col BF
+              dataElement: 'Nh894w42Ti4',
+              value: dataValue('Nh894w42Ti4')(state)
+            },
+            { //Col BG
+              dataElement: 'mZrNVMSr9QN',
+              value: dataValue('mZrNVMSr9QN')(state)
+            },
+            { //Col BH
+              dataElement: 'TVsbvQkpi17',
+              value: dataValue('TVsbvQkpi17')(state)
+            },
+            { //Col BI
+              dataElement: 'ldl5hinz5TC',
+              value: dataValue('ldl5hinz5TC')(state)
+            },
+            { //Col BJ
+              dataElement: 'mwNDjqVe6C2',
+              value: dataValue('mwNDjqVe6C2')(state)
+            },
+            { //Col BK
+              dataElement: 'ylfCWUBbiZk',
+              value: dataValue('ylfCWUBbiZk')(state)
+            },
+            { //Col BL
+              dataElement: 'jpS0aEcFGy8',
+              value: dataValue('jpS0aEcFGy8')(state)
+            },
+            { //Col BM
+              dataElement: 'LzIigcjxapS',
+              value: dataValue('LzIigcjxapS')(state)
+            },
+            { //Col BN
+              dataElement: 'VOgnzzSuYZX',
+              value: dataValue('VOgnzzSuYZX')(state)
+            },
+            { //Col BO
+              dataElement: 'YdBzuMj0CSh',
+              value: dataValue('YdBzuMj0CSh')(state)
+            },
+            { //Col BP
+              dataElement: 'XmM7gBcfg6P',
+              value: dataValue('XmM7gBcfg6P')(state)
+            },
+            { //Col BQ
+              dataElement: 'HN09RtK91MC',
+              value: dataValue('HN09RtK91MC')(state)
+            },
+
         ],
         }]
     },
   ],
-  
+
 });
