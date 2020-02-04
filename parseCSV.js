0a4074ce3ddc98e7c0027e207b07cdee952d350f
Taylor Downs <taylor@openfn.org>
2020-02-04 14:15:29 +0000

    Provide example windows path

--- parseCSV.js
+++ parseCSV.js
@@ -1,5 +1,8 @@
 parseCSV(
-  '/home/taylor/Dropbox (OpenFn)/Projects/Logical Outcomes/LOThika-Chania_farmers_Nov2019CLEAN__revised (copy).csv',
+  // '/home/taylor/Dropbox (OpenFn)/Projects/Logical Outcomes/LOThika-Chania_farmers_Nov2019CLEAN__revised (copy).csv',
+  // NOTE: I'm using double `\` to build this path when I run this on Windows.
+  // It's working at the time of this commit, on Windows 10 Pro, x64, Node v12.0.0 (via NVM 1.1.7).
+  'C:\\Users\\downs\\Dropbox (OpenFn)\\Projects\\Logical Outcomes\\LOThika-Chania_farmers_Nov2019CLEAN__revised (copy).csv'
   {
     columns: true,
     skip_empty_lines: true,
