b47c340041398b97a74daafb477f062ed0d84c41
aleksa-krolls <aleksagk@gmail.com>
2020-07-08 17:24:05 -0500

    new job for tei uploads

---
+++ createTEI__care.js
@@ -0,0 +1,10 @@
+createTEI({
+  trackedEntityType: "bsDL4dvl2ni",
+  orgUnit: "vQnvY1o8PHz",
+  attributes: [
+    {
+      attribute: "INS05jiIWB0", //Col A
+      value: dataValue("csvData.INS05jiIWB0")(state),
+    },
+  ],
+});
