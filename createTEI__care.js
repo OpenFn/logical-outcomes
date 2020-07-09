9493d5842ff0ed4f3ae0afeaeb5b06c6e2c1a686
aleksa-krolls <aleksagk@gmail.com>
2020-07-09 00:29:53 -0500

    tested version ready for UAT

--- createTEI__care.js
+++ createTEI__care.js
@@ -4,7 +4,7 @@ createTEI({
   attributes: [
     {
       attribute: "INS05jiIWB0", //Col A
-      value: dataValue("csvData.INS05jiIWB0")(state),
+      value: dataValue("csvData.Enrollment date")(state), //INS05jiIWB0
     },
     {
       attribute: "nNbMsTUf3M6", //Col B
@@ -32,84 +32,87 @@ createTEI({
     },
     {
       attribute: "aC1WkFsKqv3", //Col H
-      value: dataValue("csvData.aC1WkFsKqv3")(state),
+      //"value": '04. DEV: SRMH' //hardcoding value because of issue with options
+      value: dataValue("csvData.aC1WkFsKqv3")(state), //ERROR: Need to specify choice codes
     },
     {
       attribute: "E4D2HUQgWdJ", //Col I
-      value: dataValue("csvData.E4D2HUQgWdJ")(state),
+      value: dataValue("csvData.E4D2HUQgWdJ")(state).replace(/,/g, ""),
     },
   ],
   enrollments: [
     {
       orgUnit: "vQnvY1o8PHz",
-      program: "SHRBw9XXHFk", 
-      enrollmentDate: dataValue("csvData.Event date")(state),
-      incidentDate: dataValue("csvData.INS05jiIWB0")(state),
-    },
-  ],
-  events: [
-    {
-      program: "SHRBw9XXHFk",
-      orgUnit: "vQnvY1o8PHz",
-      eventDate: dataValue("csvData.Enrollment date")(state),
-      status: "COMPLETED",
-      storedBy: "Test",
-      //orgUnitName: dataValue('csvData.Org unit')(state),
-      dataValues: [
+      program: "SHRBw9XXHFk", //Reach Form
+      programStage: "k5vUtGga5yY", //General Info
+      enrollmentDate: dataValue("csvData.Enrollment date")(state),
+      incidentDate: dataValue("csvData.Enrollment date")(state),
+      events: [
         {
-          //Col K
-          dataElement: "mIJDiYz94qV",
-          value: dataValue("csvData.mIJDiYz94qV")(state),
-        },
-        {
-          //Col L
-          dataElement: "ICsPcYnp699",
-          value: dataValue("csvData.ICsPcYnp699")(state),
-        },
-        {
-          //Col M
-          dataElement: "KGSeDVm5WXi",
-          value: dataValue("csvData.KGSeDVm5WXi")(state),
-        },
-        {
-          //Col N
-          dataElement: "Vq1UjWgcVPR",
-          value: dataValue("csvData.Vq1UjWgcVPR")(state),
-        },
-        {
-          //Col O
-          dataElement: "qaiWE42he2W",
-          value: dataValue("csvData.qaiWE42he2W")(state),
-        },
-        {
-          //Col P
-          dataElement: "iC0QRnbMpH1",
-          value: dataValue("csvData.iC0QRnbMpH1")(state),
-        },
-        {
-          //Col Q
-          dataElement: "KO8RH3BEcIq",
-          value: dataValue("csvData.KO8RH3BEcIq")(state),
-        },
-        {
-          //Col R
-          dataElement: "XwGuR1yrQ47",
-          value: dataValue("csvData.XwGuR1yrQ47")(state),
-        },
-        {
-          //Col S
-          dataElement: "AouFSwJDulc",
-          value: dataValue("csvData.AouFSwJDulc")(state),
-        },
-        {
-          //Col T
-          dataElement: "IkCHJg0Iz16",
-          value: dataValue("csvData.IkCHJg0Iz16")(state),
-        },
-        {
-          //Col U
-          dataElement: "J9TZYmO460c",
-          value: dataValue("csvData.J9TZYmO460c")(state),
+          program: "SHRBw9XXHFk", //Reach Form
+          programStage: "k5vUtGga5yY", //General Info
+          orgUnit: "vQnvY1o8PHz",
+          eventDate: dataValue("csvData.Event date")(state),
+          status: "COMPLETED",
+          storedBy: "aleksa@openfn.org",
+          //orgUnitName: dataValue('csvData.Org unit')(state),
+          dataValues: [
+            {
+              //Col K
+              dataElement: "mIJDiYz94qV",
+              value: dataValue("csvData.mIJDiYz94qV")(state),
+            },
+            {
+              //Col L
+              dataElement: "ICsPcYnp699",
+              value: dataValue("csvData.ICsPcYnp699")(state),
+            },
+            {
+              //Col M
+              dataElement: "KGSeDVm5WXi",
+              value: dataValue("csvData.KGSeDVm5WXi")(state),
+            },
+            {
+              //Col N
+              dataElement: "Vq1UjWgcVPR",
+              value: dataValue("csvData.Vq1UjWgcVPR")(state),
+            },
+            {
+              //Col O
+              dataElement: "qaiWE42he2W",
+              value: dataValue("csvData.qaiWE42he2W")(state),
+            },
+            {
+              //Col P
+              dataElement: "iC0QRnbMpH1",
+              value: dataValue("csvData.iC0QRnbMpH1")(state),
+            },
+            {
+              //Col Q
+              dataElement: "KO8RH3BEcIq",
+              value: dataValue("csvData.KO8RH3BEcIq")(state),
+            },
+            {
+              //Col R
+              dataElement: "XwGuR1yrQ47",
+              value: dataValue("csvData.XwGuR1yrQ47")(state),
+            },
+            {
+              //Col S
+              dataElement: "AouFSwJDulc",
+              value: dataValue("csvData.AouFSwJDulc")(state),
+            },
+            {
+              //Col T
+              dataElement: "IkCHJg0Iz16",
+              value: dataValue("csvData.IkCHJg0Iz16")(state),
+            },
+            {
+              //Col U
+              dataElement: "J9TZYmO460c",
+              value: dataValue("csvData.J9TZYmO460c")(state),
+            },
+          ],
         },
       ],
     },
