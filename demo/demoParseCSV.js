63a89e221b40e0edf90c5d7a0f6444bb4bd587b1
Aleksa Krolls <aleksagk@gmail.com>
2020-11-25 22:11:30 +0200

    Create demoParseCSV.js

---
+++ demo/demoParseCSV.js
@@ -0,0 +1,3 @@
+//Job to parse message into individual rows of data
+//Post each row back to the OpenFn Inbox
+//Output should be several Messages that look like this: https://www.openfn.org/projects/1765/messages/10211878
