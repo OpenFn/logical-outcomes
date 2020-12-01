a41018d371141977ff43a2f231f90fb0d4e81b7b
Taylor Downs <taylor@openfn.org>
2020-12-01 17:39:54 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/OpenFnToDHIS2_http.js
+++ jobs/OpenFnToDHIS2_http.js
@@ -7,7 +7,7 @@ each(
       console.log(state.data);
       const attrs = { ...state.data }
       delete attrs.orgUnit;
-      return attrs;
+      return Object.entries(attrs).map(pair => ({attribute: pair[0], value: pair[1]}));
     },
     // enrollments: [] // seems as though we do not want to create enrollments
   })
