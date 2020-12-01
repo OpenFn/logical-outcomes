efa2188b4ebe3b5ce3f59e586d124f64489c4714
Taylor Downs <taylor@openfn.org>
2020-12-01 17:33:02 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/OpenFnToDHIS2_http.js
+++ jobs/OpenFnToDHIS2_http.js
@@ -4,6 +4,7 @@ each(
     trackedEntityType: 'hRqJrTjGWtg', // DHIS2 id for Person EntityType
     orgUnit: dataValue('orgUnit'),
     attributes: state => {
+      console.log(state.data);
       const attrs = { ...state.data }
       delete attrs.orgUnit;
       return attrs;
