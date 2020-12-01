c4a569f61512fff940fa2abd2e875dd01a366785
Taylor Downs <taylor@openfn.org>
2020-12-01 17:30:46 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/OpenFnToDHIS2_http.js
+++ jobs/OpenFnToDHIS2_http.js
@@ -1,42 +1,13 @@
-alterState(state => {
-  const getPersonAttributes = data => {
-    return Object.entries(data).map(item => {
-      if (item[0] === 'enrollmentDate') item[0] = 'Wj9itiV0fvP';
-      return { attribute: item[0], value: item[1] };
-    });
-  };
-
-  state.data.attributes = getPersonAttributes(state.data.csvData);
-
-  return state;
-});
-
-alterState(state => {
-  const excludeAttributeList = ['orgUnit'];
-
-  function cleanAttributes(attributes) {
-    attributes.forEach((item, index) => {
-      if (excludeAttributeList.includes(item.attribute)) {
-        console.log(
-          `${attributes[index].attribute} is not a valid attribute. Removed from state.body.attributes!`
-        );
-        attributes.splice(index, 1);
-      }
-    });
-  }
-
-  // delete state.data;
-  cleanAttributes(state.data.attributes);
-
-  return state;
-});
-
-upsertTEI({
-  trackedEntityType: 'hRqJrTjGWtg', // DHIS2 id for Person EntityType
-  orgUnit: dataValue('orgUnit'),
-  attributes: dataValue('attributes'),
-  enrollments: dataValue('enrollments'),
-});
-// alterState((state) => {
-//   return createTEI(state.body)(state);
-// });
+each(
+  dataPath('csvData'),
+  upsertTEI({
+    trackedEntityType: 'hRqJrTjGWtg', // DHIS2 id for Person EntityType
+    orgUnit: dataValue('orgUnit'),
+    attributes: state => {
+      const attrs = { ...state.data }
+      delete attrs.orgUnit;
+      return attrs;
+    },
+    // enrollments: [] // seems as though we do not want to create enrollments
+  })
+);
