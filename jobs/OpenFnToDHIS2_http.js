7bd0b23a1a9836f999b9f9582636e5005946754c
Chaiwa Berian <Chaiwa Berian>
2020-12-02 07:49:55 +0200

    Logs TEI returned by GET

--- jobs/OpenFnToDHIS2_http.js
+++ jobs/OpenFnToDHIS2_http.js
@@ -4,16 +4,17 @@ each(
     trackedEntityType: 'hRqJrTjGWtg', // DHIS2 id for Person EntityType
     orgUnit: dataValue('orgUnit'),
     attributes: state => {
-      const attrs = { ...state.data, 'Wj9itiV0fvP': state.data.enrollmentDate };
+      const attrs = { ...state.data, Wj9itiV0fvP: state.data.enrollmentDate };
       delete attrs.orgUnit;
       delete attrs.enrollmentDate;
-      
+
       const attributePairs = Object.entries(attrs).map(pair => ({
-        attribute: pair[0], value: pair[1]
+        attribute: pair[0],
+        value: pair[1],
       }));
-      
+
       return attributePairs;
     },
-    // enrollments: [] // seems as though we do not want to create enrollments
+    enrollments: [], // seems as though we do not want to create enrollments
   })
 );
