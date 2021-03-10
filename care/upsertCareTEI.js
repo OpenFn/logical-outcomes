ff71df39d0ef9132e08218cef0ec28fae8ec764b
Aleksa Krolls <aleksagk@gmail.com>
2021-03-10 10:18:37 +0200

    aleksa@openfn.org's change on OpenFn.org

--- care/upsertCareTEI.js
+++ care/upsertCareTEI.js
@@ -1,20 +1,22 @@
 //TO UPDATE: https://dev.piirs.care.org/dhis-web-tracker-capture/index.html#/dashboard?tei=J7t4VZTgb1N&program=SHRBw9XXHFk&ou=gixv5Y8nbzp
-each("$.csvData[*]",
+each(
+  dataPath("csvData[*]"),
   upsertTEI(
-  'SgQW3vpnhuL', //piirs uid
-  {
-    trackedEntityType: 'bsDL4dvl2ni',
-    orgUnit: dataValue('OrgUnit'),
-    attributes: [
-      {
-        attribute: 'aX5hD4qUpRW',
-        value: dataValue('aX5hD4qUpRW'),
-      },
-      {
-        attribute: 'MxQPuS9G7hh',
-        value: dataValue('MxQPuS9G7hh'),
-      },
-    ],
-  },
-  { strict: false }
-));
+    'aX5hD4qUpRW', //piirs uid
+    {
+      trackedEntityType: 'bsDL4dvl2ni',
+      orgUnit: dataValue('OrgUnit'),
+      attributes: [
+        {
+          attribute: 'aX5hD4qUpRW',
+          value: dataValue('aX5hD4qUpRW'),
+        },
+        {
+          attribute: 'MxQPuS9G7hh',
+          value: dataValue('MxQPuS9G7hh'),
+        },
+      ],
+    },
+    { strict: false }
+  )
+);
