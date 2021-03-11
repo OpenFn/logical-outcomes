dd44c16053430eb804a9c754beb8d8ef3b4e817b
Aleksa Krolls <aleksagk@gmail.com>
2021-03-11 22:37:48 +0200

    aleksa@openfn.org's change on OpenFn.org

--- care/upsertCareTEI.js
+++ care/upsertCareTEI.js
@@ -1,6 +1,10 @@
 //TO UPDATE: https://piirs.care.org/dhis-web-tracker-capture/index.html#/dashboard?tei=J7t4VZTgb1N&program=SHRBw9XXHFk&ou=gixv5Y8nbzp
 each(
   dataPath("csvData[*]"),
+  alterState(state => {
+    console.log(state.data);
+    return state; 
+  }),
   upsertTEI(
     'SgQW3vpnhuL', //piirs uid
     {
