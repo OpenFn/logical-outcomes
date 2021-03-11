20d1d72db98761affcdac61ea1ad8e0453346c55
Aleksa Krolls <aleksagk@gmail.com>
2021-03-11 22:44:04 +0200

    aleksa@openfn.org's change on OpenFn.org

--- care/upsertCareTEI.js
+++ care/upsertCareTEI.js
@@ -4,15 +4,15 @@ each("$.csvData[*]",
     'SgQW3vpnhuL', //piirs uid
     {
       trackedEntityType: 'bsDL4dvl2ni',
-      orgUnit: dataValue('OrgUnit'),
+      orgUnit: dataValue('OrgUnit')(state),
       attributes: [
         {
           attribute: 'SgQW3vpnhuL',
-          value: dataValue('SgQW3vpnhuL'),
+          value: dataValue('SgQW3vpnhuL')(state),
         },
         {
           attribute: 'MxQPuS9G7hh',
-          value: dataValue('MxQPuS9G7hh'),
+          value: dataValue('MxQPuS9G7hh')(state),
         },
       ],
     },
