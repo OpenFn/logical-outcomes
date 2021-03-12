b37eaebd0e7e1cd0a27b52dfc96bdbe23c84a319
Aleksa Krolls <aleksagk@gmail.com>
2021-03-12 20:58:42 +0200

    georgi@logicaloutcomes.net's change on OpenFn.org

--- care/upsertCareTEI.js
+++ care/upsertCareTEI.js
@@ -1,4 +1,4 @@
-//TO UPDATE: https://piirs.care.org/dhis-web-tracker-capture/index.html#/dashboard?tei=J7t4VZTgb1N&program=SHRBw9XXHFk&ou=gixv5Y8nbzp
+//TO UPDATE: https://dev.piirs.care.org/dhis-web-tracker-capture/index.html#/dashboard?tei=J7t4VZTgb1N&program=SHRBw9XXHFk&ou=gixv5Y8nbzp
 alterState(state => {
   const { csvData } = state.data;
   
